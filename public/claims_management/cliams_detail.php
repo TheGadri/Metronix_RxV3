2aea5752f87dc920a3aa329a6db552c3bb8ba1df
fiifi_gh <francisk994@gmail.com>
2019-09-02 20:21:30 +0000

    complete list of claim managment

--- public/claims_management/cliams_detail.php
+++ public/claims_management/cliams_detail.php
@@ -3,17 +3,19 @@
         <div class="table-responsive" style="max-height:500px">
             <div class="kt-portlet__body kt-portlet__body--fit">
                 <!--begin: Datatable -->
-                <div class="kt-datatable kt-datatable--default kt-datatable--brand kt-datatable--loaded" id="local_data" style="">
+                <div class="kt-datatable kt-datatable--default kt-datatable--brand kt-datatable--loaded" id="local_data"
+                    style="">
                     <table class="table">
                         <tbody class="kt-datatable__body bg-green">
                             <tr data-row="0" class="kt-datatable__row" style="left: 0px; ">
                                 <td data-field="consult_date" class="kt-datatable__cell no_v">
-                                    <span class="kt-font-bold">Claim Auditied :</span>
-                                    <span>&nbsp;AOA 1,200,000.00</span>
+                                    <button class="btn btn-sm btn-warning text-white">Approve All Items</button>
+                                    <button class="btn btn-sm btn-danger">Reject All Items</button>
+                                    <button class="btn btn-sm btn-light">Financial Auditing</button>
                                 </td>
                                 <td data-field="facility_name" class="kt-datatable__cell no_v">
-                                    <span class="kt-font-bold">Invoice No :</span>
-                                    <span>&nbsp;AOA 1,200,000.00</span>
+                                    <button class="btn btn-sm btn-warning">View Medical History</button>
+                                    <button class="btn btn-sm btn-light">Print</button>
                                 </td>
                             </tr>
                             <tr data-row="0" class="kt-datatable__row" style="left: 0px; ">
@@ -31,14 +33,18 @@
                                     <span class="kt-font-bold">Visit Type: </span>
                                     <span>&nbsp;Out-Patient</span>
                                 </td>
-                                <td></td>
+                                <td>
+                                    <span class="kt-font-bold">Invoice No: </span>
+                                    <span>&nbsp;Paulina Laukino</span>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <!--end: Datatable -->
             </div>
-            <div class="kt-datatable kt-datatable--default kt-datatable--brand kt-datatable--loaded" id="local_data" style="">
+            <div class="kt-datatable kt-datatable--default kt-datatable--brand kt-datatable--loaded" id="local_data"
+                style="">
                 <table class="table">
                     <colgroup>
                         <col>
@@ -55,11 +61,11 @@
 
                         <col span="3" class="light-red">
 
-                        <col class="light-blue">
+                        <col span="2" class="light-blue">
 
-                        <col class="light-white-pink">
+                        <col span="2" class="light-white-pink">
 
-                        <col span="3" class="light-white-green">
+                        <col span="2" class="light-white-green">
 
                     </colgroup>
                     <thead class="kt-datatable__head">
@@ -72,10 +78,10 @@
                             <th class="kt-datatable__cell kt-datatable__cell--sort"></th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort text-center" colspan="3">Claimed
                             </th>
-                            <th class="kt-datatable__cell kt-datatable__cell--sort text-center">Discount
+                            <th colspan="2" class="kt-datatable__cell kt-datatable__cell--sort text-center">Discount
                             </th>
-                            <th class="kt-datatable__cell kt-datatable__cell--sort">Co-Payment</th>
-                            <th class="kt-datatable__cell kt-datatable__cell--sort text-center" colspan="3">Awarded</th>
+                            <th colspan="2" class="kt-datatable__cell kt-datatable__cell--sort">Co-Payment</th>
+                            <th class="kt-datatable__cell kt-datatable__cell--sort text-center" colspan="2">Awarded</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort"></th>
                         </tr>
                         <tr>
@@ -88,8 +94,10 @@
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Qty</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Price</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Total</th>
-                            <th class="kt-datatable__cell kt-datatable__cell--sort">%Amount</th>
-                            <th class="kt-datatable__cell kt-datatable__cell--sort">%Amount</th>
+                            <th class="kt-datatable__cell kt-datatable__cell--sort">%</th>
+                            <th class="kt-datatable__cell kt-datatable__cell--sort">Amount</th>
+                            <th class="kt-datatable__cell kt-datatable__cell--sort">%</th>
+                            <th class="kt-datatable__cell kt-datatable__cell--sort">Amount</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Qty</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Price</th>
                             <th class="kt-datatable__cell kt-datatable__cell--sort">Total</th>
@@ -108,14 +116,33 @@
                             <td class="kt-datatable__cell"><span>1</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span class="kt-font-bold kt-font-success">Approved</span>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><span class="kt-font-bold">0.00</span></td>
+                            <td class="kt-datatable__cell">
+                                <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option value="">No Decision</option>
+                                    <option value="">Approve</option>
+                                    <option value="">Reject</option>
+                                    <option value="">Dispute</option>
+                                </select>
                             </td>
-                            <td class="kt-datatable__cell"><span></span></td>
+                            <td class="kt-datatable__cell">
+                            <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option selected disabled>Select Reason (if any)</option>
+                                    <option value="">Exceeding Accomodation Limit</option>
+                                    <option value="">Exceeding Delivery Limit</option>
+                                    <option value="">Exceeding Dental Limit</option>
+                                    <option value="">Exceeding Optical Limit</option>
+                                    <option value="">Not Covered</option>
+                                    <option value="">Not Indicated</option>
+                                    <option value="">Other Reason</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr class="kt-datatable__row">
                             <td class="kt-datatable__cell"><span>Normal Delivery</span></td>
@@ -127,13 +154,33 @@
                             <td class="kt-datatable__cell"><span>1</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span class="kt-font-bold text-error">Decline</span></td>
-                            <td class="kt-datatable__cell"><span></span></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><span class="kt-font-bold">0.00</span></td>
+                            <td class="kt-datatable__cell">
+                                <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option value="">No Decision</option>
+                                    <option value="">Approve</option>
+                                    <option value="">Reject</option>
+                                    <option value="">Dispute</option>
+                                </select>
+                            </td>
+                            <td class="kt-datatable__cell">
+                            <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option selected disabled>Select Reason (if any)</option>
+                                    <option value="">Exceeding Accomodation Limit</option>
+                                    <option value="">Exceeding Delivery Limit</option>
+                                    <option value="">Exceeding Dental Limit</option>
+                                    <option value="">Exceeding Optical Limit</option>
+                                    <option value="">Not Covered</option>
+                                    <option value="">Not Indicated</option>
+                                    <option value="">Other Reason</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr class="kt-datatable__row">
                             <td class="kt-datatable__cell"><span>Normal Delivery</span></td>
@@ -145,24 +192,47 @@
                             <td class="kt-datatable__cell"><span>1</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
                             <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span>1,200,000.00</span></td>
-                            <td class="kt-datatable__cell"><span class="kt-font-bold kt-font-success">Approved</span>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:60px;"></td>
+                            <td class="kt-datatable__cell"><input placeholder="0" type="number" class="form-control form-control-sm" style="width:80px;"></td>
+                            <td class="kt-datatable__cell"><span class="kt-font-bold">0.00</span></td>
+                            <td class="kt-datatable__cell">
+                                <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option value="">No Decision</option>
+                                    <option value="">Approve</option>
+                                    <option value="">Reject</option>
+                                    <option value="">Dispute</option>
+                                </select>
                             </td>
-                            <td class="kt-datatable__cell"><span></span></td>
+                            <td class="kt-datatable__cell">
+                            <select name="" id="" class="form-control form-control-sm" style="width:100px;">
+                                    <option selected disabled>Select Reason (if any)</option>
+                                    <option value="">Exceeding Accomodation Limit</option>
+                                    <option value="">Exceeding Delivery Limit</option>
+                                    <option value="">Exceeding Dental Limit</option>
+                                    <option value="">Exceeding Optical Limit</option>
+                                    <option value="">Not Covered</option>
+                                    <option value="">Not Indicated</option>
+                                    <option value="">Other Reason</option>
+                                </select>
+                            </td>
                         </tr>
+
+
                     </tbody>
                     <tfoot class="table-secondary kt-datable__foot">
                         <tr class="kt-datatable__row">
                             <td class="kt-datatable__cell text-center" colspan="6"><span class="kt-font-bold">Total
                                     Amount</span></td>
-                            <td class="kt-datatable__cell text-right" colspan="3"><span class="kt-font-bold">00.00</span></td>
+                            <td class="kt-datatable__cell text-right" colspan="3"><span
+                                    class="kt-font-bold">00.00</span></td>
                             <td class="kt-datatable__cell text-center"><span class="kt-font-bold">00.00</span></td>
                             <td class="kt-datatable__cell text-center"><span class="kt-font-bold">00.00</span></td>
-                            <td class="kt-datatable__cell text-right" colspan="3"><span class="kt-font-bold">00.00</span>
+                            <td class="kt-datatable__cell text-right" colspan="3"><span
+                                    class="kt-font-bold">00.00</span>
                             </td>
                             <td class="kt-datatable__cell text-right" colspan="3"><span class="kt-font-bold"></span>
                             </td>
